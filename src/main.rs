30b0de9ae405186f64ea144de32a1d56e6ee2401
sgulan <sinancangulan@gmail.com>
2025-04-25 22:53:40 -0400

    Implement mouse and wheel distance tracking, use rdev::EventType in crossbeam_channel

--- src/main.rs
+++ src/main.rs
@@ -4,34 +4,51 @@ use std::thread;
 
 #[derive(Default)]
 struct State {
-    mouse_moves: usize,
-    wheel_spins: usize,
+    mouse_distance: usize,
+    wheel_distance: usize,
     button_presses: usize,
     key_presses: usize,
-}
-
-enum Msg {
-    MouseMove,
-    WheelSpin,
-    ButtonPress,
-    KeyPress,
+    // TODO: key and button press total durations
+    // potential TODO: per-key/button stats (e.g. clicked enter x times)
 }
 
 fn main() {
     // 1. spawn the worker that *owns* State
-    let (tx, rx) = unbounded::<Msg>();
+    let (tx, rx) = unbounded::<rdev::EventType>();
     thread::spawn(move || {
         let mut s = State::default();
-        for msg in rx {
-            match msg {
-                Msg::MouseMove => s.mouse_moves += 1,
-                Msg::WheelSpin => s.wheel_spins += 1,
-                Msg::ButtonPress => s.button_presses += 1,
-                Msg::KeyPress => s.key_presses += 1,
+        let mut first_mouse_move = true;
+        let mut last_mouse_pos = (0.0, 0.0);
+        for event in rx {
+            match event {
+                EventType::MouseMove { x, y } => {
+                    let mut distance = 0.0;
+                    if !first_mouse_move {
+                        distance = ((x - last_mouse_pos.0).powi(2)
+                            + (y - last_mouse_pos.1).powi(2))
+                        .sqrt();
+                        println!(
+                            "Mouse moved from ({}, {}) to ({}, {}), Distance: {}",
+                            last_mouse_pos.0, last_mouse_pos.1, x, y, distance
+                        );
+                        last_mouse_pos = (x, y);
+                    } else {
+                        last_mouse_pos = (x, y);
+                        first_mouse_move = false;
+                    }
+                    s.mouse_distance += distance as usize;
+                }
+                EventType::Wheel { delta_x, delta_y } => {
+                    s.wheel_distance += (delta_x.abs() + delta_y.abs()) as usize;
+                    println!("Wheel moved by ({}, {})", delta_x, delta_y);
+                }
+                EventType::ButtonPress(_) => s.button_presses += 1,
+                EventType::KeyPress(_) => s.key_presses += 1,
+                _ => {}
             }
             println!(
-                "Mouse Moves: {}, Wheel Spins: {}, Button Presses: {}, Key Presses: {}",
-                s.mouse_moves, s.wheel_spins, s.button_presses, s.key_presses
+                "Mouse Distance: {}, Wheel Spins: {}, Button Presses: {}, Key Presses: {}",
+                s.mouse_distance, s.wheel_distance, s.button_presses, s.key_presses
             );
 
             // TODO: down the line, we will log to a DB or file periodically (every N messages/seconds)
@@ -43,20 +60,6 @@ fn main() {
     listen(callback).unwrap();
 }
 
-fn send_event(tx: &Sender<Msg>, ev: &Event) {
-    match ev.event_type {
-        EventType::MouseMove { x, y } => {
-            tx.send(Msg::MouseMove).ok();
-        }
-        EventType::Wheel { delta_x, delta_y } => {
-            tx.send(Msg::WheelSpin).ok();
-        }
-        EventType::ButtonPress(_) => {
-            tx.send(Msg::ButtonPress).ok();
-        }
-        EventType::KeyPress(_) => {
-            tx.send(Msg::KeyPress).ok();
-        }
-        _ => {}
-    }
+fn send_event(tx: &Sender<EventType>, ev: &Event) {
+    tx.send(ev.event_type.clone()).ok();
 }
