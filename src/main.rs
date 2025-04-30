ad388b145c747cfe8b6ad86d4a32355629645287
sinancang <sinancangulan@gmail.com>
2025-04-29 21:50:35 -0400

    Implement periodic logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use crossbeam_channel::{Sender, unbounded};
 use rdev::{Event, EventType, listen};
 use std::thread;
-
+use std::sync::{Arc, Mutex};
+use chrono::Local;
 use clap::Parser;
 use serde::Deserialize;
 use std::fs;
@@ -37,18 +38,51 @@ fn main() {
     let args = Cli::parse();
     let config_contents = fs::read_to_string(args.config).unwrap();
     let config: Config = toml::from_str(&config_contents).unwrap();
-    println!("Logging to {}", config.logging.path);
+    println!("Logging to {} every {} milliseconds", config.logging.path, config.logging.period_ms);
 
+    let log_path = config.logging.path.clone();
+    let log_period = config.logging.period_ms;
+
+    let state = Arc::new(Mutex::new(State::default()));
+    let state_clone = Arc::clone(&state);
+
+    // logger thread
+    thread::spawn(move || {
+        loop {
+            thread::sleep(std::time::Duration::from_millis(log_period as u64));
+
+            let mut s = state_clone.lock().unwrap();
+            let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+            let log = format!(
+                "[{}] Mouse Distance: {}, Wheel Spins: {}, Button Presses: {}, Key Presses: {}\n",
+                timestamp, s.mouse_distance, s.wheel_distance, s.button_presses, s.key_presses
+            );
+    
+            std::fs::OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(&log_path)
+                .and_then(|mut f| std::io::Write::write_all(&mut f, log.as_bytes()))
+                .expect("Failed to write log");
+    
+            // Reset counters
+            println!("Resetting state after logging");
+            *s = State::default();
+        }
+    });
+
+    // event listener thread
     let (tx, rx) = unbounded::<rdev::EventType>();
     thread::spawn(move || {
-        let mut s = State::default();
         let mut first_mouse_move = true;
         let mut last_mouse_pos = (0.0, 0.0);
         for event in rx {
+            let mut s = state.lock().unwrap();
             process_event(event, &mut s, &mut first_mouse_move, &mut last_mouse_pos);
         }
     });
 
+    // callback for event listener
     let callback = move |ev: Event| send_event(&tx, &ev);
     listen(callback).unwrap();
 }
